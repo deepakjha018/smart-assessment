6e739afc2583c724c1bf7a003aec2d2da31e7568
deepakjha018 <deepak2004jha@gmail.com>
2026-03-17 23:06:57 +0530

    connected postgres database

--- smart_assessment/settings.py
+++ smart_assessment/settings.py
@@ -81,12 +81,13 @@ WSGI_APPLICATION = 'smart_assessment.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+import os
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600
+    )
 }
 
 
